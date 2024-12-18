fe156987f376ba846bd61b0deec4a0cfef3cb959
Ala Gharbi <gharbiala159@gmailcom>
2024-12-18 16:36:55 +0100

    Setting Up Tailwind css And backend Folders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-blue-500">Welcome to my Project.....</h1>
+      <h1 className="text-blue-500">Welcome to my Project Mern stack.....</h1>
     </>
   );
 }
